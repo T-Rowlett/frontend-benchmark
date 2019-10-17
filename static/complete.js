9b117d86bbc489b7f3c0058db020faab428b47a8
Student <student@donation.local>
2019-10-17 08:29:29 -0500

    attempt

---
+++ static/complete.js
@@ -0,0 +1,17 @@
+const entrie = document.querySelector("#input")
+const entries = document.querySelector("#text-input")
+
+function updateentrie(data) {
+    const options = []
+    for (const message of data.content) {
+        options.push(`<option value="${message}">`);
+    }
+    entries.innerHTML = options.join("");
+}
+entrie.addEventListener("input", () => {
+    const url = `/api/input?q=${entrie.value}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(updateentrie);
+});
